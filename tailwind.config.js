9f65510a6dd247b4d941b861c5687d7b0cd60ad9
nwebpro <nwebpro.work@gmail.com>
2022-10-31 10:03:09 +0600

    tailwind config setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,36 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1170px'
+    },
     extend: {
       colors: {
         'theme-default': '#FF3811',
         'theme-dark': '#151515'
+      },
+      fontFamily: {
+        'lato': ['Lato', 'sans-serif'],
+      },
+      spacing: {
+        'b-130': '130px',
+      },
+      borderRadius: {
+        'rounded-10': '10px',
+      },
+      lineHeight: {
+        'leading-75': '75px',
+        'leading-54': '54.46px',
+        'leading-30': '30.26px',
+        'leading-21': '21.78px',
+      },
+      fontSize: {
+        'text-45': '45px',
+        'text-30': '30px',
+        'text-25': '25px',
       }
     },
   },
